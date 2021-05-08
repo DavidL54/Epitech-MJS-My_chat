d04f1730e36044ff5f73df4bbe3319020dd8ce26
Elian nicaise <elian.nicaise@epitech.eu>
2021-05-08 20:56:59 +0200

    add(newLink): create a route for send a new link for confirmation email

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -150,7 +150,6 @@ router.get('/confirmation/:email/:token', (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) => {
-    console.log(req.body);
     User.find({ "$or": [{ email: req.body.username }, { username: req.body.username }] })
         .exec()
         .then(user => {
@@ -166,7 +165,7 @@ router.post('/login', (req, res, next) => {
                 }
                 console.log(user)
                 if (!user[0].active) {
-                    res.status(401).json(
+                    return res.status(401).json(
                         'Your Email has not been verified. Please click on resend'
                     )
                 }
@@ -195,6 +194,47 @@ router.post('/login', (req, res, next) => {
             });
         })
 });
+
+router.get('/newlink', (req, res, next) => {
+    User.find({ "$or": [{ email: req.body.username }, { username: req.body.username }] })
+        .exec()
+        .then(user => {
+        // user is not found into database
+        console.log(user)
+        if (!user) {
+            return res.status(400).send({msg:'We were unable to find a user with that email. Make sure your Email is correct!'});
+        }
+        // user has been already verified
+        else if (user[0].active) {
+            return res.status(200).send('This account has been already verified. Please log in.');
+        }
+        // send verification link
+        else {
+            // generate token and save
+            const token = new Token({ _userId: user[0]._id, token: crypto.randomBytes(16).toString('hex') });
+            token.save(function (err) {
+                if (err) {
+                    return res.status(500).send({msg:err.message});
+                }
+                // Send email (use credintials of SendGrid)
+                const transporter = nodemailer.createTransport({ service: 'Gmail', auth: { user: process.env.GMAIL_USERNAME, pass: process.env.GMAIL_PASSWORD } });
+                const mailOptions = {
+                    from: 'd.e.scord5499@gmail.com',
+                    to: user[0].email,
+                    subject: 'Account Verification Link',
+                    text: 'Hello ' + req.body.name + ',\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/user\/confirmation\/' + user[0].email + '\/' + token.token + '\n\nThank You!\n'
+                };
+                transporter.sendMail(mailOptions, function (err) {
+                    if (err)
+                        return res.status(500).send('Technical Issue!, Please click on resend for verify your Email.');
+                    return res.status(200).send('A verification email has been sent to ' + user[0].email + '. It will be expire after one day. If you not get verification Email click on resend token.');
+                });
+            });
+        }
+    });
+})
+
+
 
 router.delete('/:userId', checkAuth, (req, res, next) => {
     User.remove({ id: req.params.userId })
