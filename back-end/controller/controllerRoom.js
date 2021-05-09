959e773b94a4ef8848d6777d49a30fad6157098f
Elian nicaise <elian.nicaise@epitech.eu>
2021-05-09 04:09:50 +0200

    add(Room): create route in db

--- back-end/controller/controllerRoom.js
+++ back-end/controller/controllerRoom.js
@@ -1,4 +1,5 @@
 const Room = require('../models/modelRoom');
+const User = require('../models/modelUser');
 
 exports.getAll = (req, res) => {
     Room.find({}).lean().exec()
@@ -10,17 +11,35 @@ exports.getAll = (req, res) => {
                 return res.status(200).json(room);
             }
      });
-           
+
 };
 
 exports.createRoom = (req, res) => {
-    const room = new Room({
-        
-    });
+    Room.find({name: req.body.name})
+        .lean()
+        .exec()
+        .then(room => {
+            if (room.length >= 1) {
+                return res.status(409).json(
+                    'Room exists'
+                );
+            } else {
+                const room = new Room({
+                    roomAdmin: req.userData.userId,
+                    name: req.body.name
+                });
+                room.save()
+                    .then(result => {
+                        return res.status(200).json({
+                            message: "Room created"
+                        })
+                    });
+            }
+        });
 };
 
 exports.updateRoom = (req, res) => {
-  
+
 };
 
 exports.deleteRoom = (req, res) => {
