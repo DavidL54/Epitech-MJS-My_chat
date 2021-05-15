84045f0ba8585b8dca66cda0b5365d9bf7cf005d
Elian nicaise <elian.nicaise@epitech.eu>
2021-05-16 01:29:36 +0200

    initial commit for test branch

--- back-end/config.js
+++ back-end/config.js
@@ -1,5 +1,7 @@
 const config = {
-	"DBHost": "mongodb://mongo:27017/mychat",
+	// "DBHost": "mongodb://mongo:27017/mychat",
+	"DBHost": "mongodb://127.0.0.1:27017/my_test_database_app",
+
 	"GMAIL_USERNAME" : "d.e.scord5499@gmail.com",
 	"GMAIL_PASSWORD": "d.e.p4$5W0rd549998",
 	"JWT_KEY": "secret",
