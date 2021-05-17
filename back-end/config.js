c2d6e69f28e53e87c8d6861973c3b339e8f48b77
David <david.leroy@epitech.eu>
2021-05-18 01:58:50 +0200

    small fix

--- back-end/config.js
+++ back-end/config.js
@@ -1,10 +1,16 @@
+
+const istest = false;
+
+const mongourl = istest ? '127.0.0.1' : 'mongo';
+const rabbiturl = istest ? '127.0.0.1' : 'rabbitmq';
+
 const config = {
-	"DBHost": "mongodb://127.0.0.1:27017/my_test_database_app",
+	"DBHost": `mongodb://${mongourl}:27017/mychat`,
 	"GMAIL_USERNAME" : "d.e.scord5499@gmail.com",
 	"GMAIL_PASSWORD": "d.e.p4$5W0rd549998",
 	"JWT_KEY": "secret",
 	"JWT_KEY_RESET": "secret1234",
-	"RABBITURL": "amqp://127.0.0.1:5672?heartbeat=60",
+	"RABBITURL": `amqp://${rabbiturl}:5672?heartbeat=60`,
 }
 
 module.exports = config;
